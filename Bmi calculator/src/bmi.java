6c7648a22c923d76913140a370071aff564837ff
Andrew <Andrew@LAPTOP-HGD6TG36.fios-router.home>
2020-09-19 10:20:53 -0400

    cleaned up code and added joption to final result. still crashes if no value is entered
    Change-Id: Ibb09c1e81b792bd61da1982a2cf25b8cf5e7e678

--- Bmi calculator/src/bmi.java
+++ Bmi calculator/src/bmi.java
@@ -9,58 +9,58 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 public class bmi {
 
-	public static void main(String[] args) {
+	        public static void main(String[] args) {
 		
-		JTextField feet = new JTextField(5);
-    JTextField inches = new JTextField(5);
+		    JTextField feet = new JTextField(5);
+            JTextField inches = new JTextField(5);
 
-    JPanel myPanel = new JPanel();
-    myPanel.add(new JLabel("feet:"));
-    myPanel.add(feet);
-    myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-    myPanel.add(new JLabel("inches:"));
-    myPanel.add(inches);
+            JPanel myPanel = new JPanel();
+            myPanel.add(new JLabel("feet:"));
+            myPanel.add(feet);
+            myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+            myPanel.add(new JLabel("inches:"));
+            myPanel.add(inches);
 
-    	int result = JOptionPane.showConfirmDialog(null, myPanel, 
+    	    int result = JOptionPane.showConfirmDialog(null, myPanel, 
              "How tall are you", JOptionPane.OK_CANCEL_OPTION);
-    	if (result == JOptionPane.OK_OPTION) {
+    	    if (result == JOptionPane.OK_OPTION) {
     		
     		
     		{
-    	   	System.out.println(result);
+    	   	 System.out.println(result);
     	   	
-    	   	String feetMem= feet.getText();
-    	   	int feet1 = Integer.parseInt(feetMem);
+    	   	 String feetMem= feet.getText();
+    	   	 int feet1 = Integer.parseInt(feetMem);
     	  
     	   	
     	   
-    	   	String inchesMem= inches.getText();
-    	   	int inches1 = Integer.parseInt(inchesMem); //if you enter zero, it crashes
+    	      String inchesMem= inches.getText();
+    	   	  int inches1 = Integer.parseInt(inchesMem); //if you enter zero, it crashes
     	   	
-    	   	//JTextField feet = new JTextField("-5");
-    	  // 	String text = feet.getText();
-    	   	//int xaxis1 = Integer.parseInt(text);
-    	   	
+    	    	//JTextField feet = new JTextField("-5");
+    	      // 	String text = feet.getText();
+    	      //int xaxis1 = Integer.parseInt(text);
+    	    	
 		
-		//double height= Integer.parseInt(JOptionPane.showInputDialog("how tall are you in inches"));
+		       //double height= Integer.parseInt(JOptionPane.showInputDialog("how tall are you in inches"));
 		       
 		        
-		        double weight= Integer.parseInt (JOptionPane.showInputDialog( "how much do you weigh"));
+		       double weight= Integer.parseInt (JOptionPane.showInputDialog( "how much do you weigh"));
 		        
-		    //this is a place holder, its not working
-		   int footMeasurement= 12;
+		       //this is a place holder, its not working
+		       int footMeasurement= 12;
 		        
 		        
-		        int adjustedHeight=   feet1 * footMeasurement;
-		        int adjustedHeight2= adjustedHeight + inches1;
+		       int adjustedHeight=   feet1 * footMeasurement;
+		       int adjustedHeight2= adjustedHeight + inches1;
 		
 		        
-		        double bmiWeight= 703* weight;
+		       double bmiWeight= 703* weight;
 		       double bmiHeight= (int) Math.pow(adjustedHeight2, 2);
 		       double bmiFinal= bmiWeight/bmiHeight;
 		      
 		        
-		        System.out.println (bmiFinal);
+		       JOptionPane.showMessageDialog(null, bmiFinal);
 		        
 		      
 		        
