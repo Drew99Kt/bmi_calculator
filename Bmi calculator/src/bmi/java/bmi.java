041b2b54dbc4ed04e64058a10d252202e2346683
Andrew <Andrew@LAPTOP-HGD6TG36.fios-router.home>
2020-09-22 02:42:53 -0400

    cleaned up code and did comments
    Change-Id: I7ca94adb2893db18e6a8f195c4260a10f8408fd5

--- Bmi calculator/src/bmi/java/bmi.java
+++ Bmi calculator/src/bmi/java/bmi.java
@@ -25,55 +25,65 @@ public class bmi implements ActionListener {
 
  public static void main(String[] args) {
 	 
-	  panel = new JPanel();
+	 panel = new JPanel();
 	 
+	 //frame build
 	 frame = new JFrame("Bmi Calculator");
 	 frame.setBounds(400,50,10,50);
 	 frame.setSize (370,300);
 	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 
 	 frame.add(panel);
-	 
 	 
 	 panel.setLayout(null);
 	 
-	  feetLabel= new JLabel ("Feet",SwingConstants.CENTER);
+	 //feet label
+	 feetLabel= new JLabel ("Feet",SwingConstants.CENTER);
 	 feetLabel.setBounds(10,40,80,25);
 	 panel.add(feetLabel);
 	 
-	  feetText = new JTextField ();
+	 //feet text
+	 feetText = new JTextField ();
 	 feetText.setBounds(100,40,165,25);
 	 panel.add(feetText);
 	 
-	  inchesLabel = new JLabel ("Inches" ,SwingConstants.CENTER);
+	 //inches label
+	 inchesLabel = new JLabel ("Inches" ,SwingConstants.CENTER);
 	 inchesLabel.setBounds(10,70, 80, 25);
-	 
 	 panel.add(inchesLabel);
 	
 	 
-	  inchesText = new JTextField ("0");
+	 //inches text
+	 inchesText = new JTextField ("0");
 	 inchesText.setBounds(100,70,165,25);
 	 panel.add(inchesText);
 	 
-	 	weightlabel = new JLabel ("weight",SwingConstants.CENTER);
-		weightlabel.setBounds(10,100, 80, 25);
-		panel.add(weightlabel);
-		
-		  weightText = new JTextField ();
-		  weightText.setBounds(100,100,165,25);
-		  panel.add(weightText);
+	 
+	 //weight label
+	 weightlabel = new JLabel ("weight",SwingConstants.CENTER);
+	 weightlabel.setBounds(10,100, 80, 25);
+	 panel.add(weightlabel);
 		
 	 
+	 //weight test
+	 weightText = new JTextField ();
+	 weightText.setBounds(100,100,165,25); 
+	 panel.add(weightText);
+		
+	 
+	 //Sumbit button
 	 button= new JButton ("Sumbit");
 	 button.setBounds(100, 140, 80, 25);
 	 button.addActionListener(new bmi());
 	 panel.add(button);
 	 
 	 
+	 //Weight label
 	 weightlabel = new JLabel ("Enter your height and weight");
 	 weightlabel.setBounds(100,5, 300, 25);
 	 panel.add(weightlabel);
 	 
+	 
+	 //this is the output after the math runs
 	 bmiFinalLabel=new JLabel("");
      bmiFinalLabel.setBounds(80, 180,300,25);
 	 panel.add(bmiFinalLabel);
@@ -93,8 +103,8 @@ public class bmi implements ActionListener {
 
 @Override
 public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
 	
+	// this makes sure user knows they are leaving blanks fields.  no checks if letters are entered
 	 if (feetText.getText().trim().isEmpty()) {
 	    	bmiFinalLabel.setText("The Field for feet wasn't entered");
 	    } else if (inchesText.getText().trim().isEmpty()) {
@@ -103,30 +113,34 @@ public void actionPerformed(ActionEvent e) {
 	    	bmiFinalLabel.setText("The Field for weight wasn't entered");
 	    }
 	
+	//converting string to int
 	String feetmem = feetText.getText();
 	int feetnum = Integer.parseInt(feetmem);
 	
+	//converting string to int
 	String inchesmem = inchesText.getText();
 	int inchesnum = Integer.parseInt(inchesmem);
 	
+	//converting string to int
 	String weightmem = weightText.getText();
 	int weightnum = Integer.parseInt(weightmem);
 	
 	
+	//start of bmi math
 	int footMeasurement= 12;
-      
       
     int adjustedHeight=   feetnum * footMeasurement;
     int adjustedHeight2= adjustedHeight + inchesnum;
-
-       
+  
     double bmiWeight= 703* weightnum;
+    
     double bmiHeight= (int) Math.pow(adjustedHeight2, 2);
+    
     int bmiFinal= (int) (bmiWeight/bmiHeight);
-	
+	//end of math
    
     
-    
+    //Bmi results
     if (bmiFinal >= 0 && bmiFinal <= 18.5) {
     	 bmiFinalLabel.setText(String.valueOf("YourBmi is "+bmiFinal+ ", your underweight"));
  	
@@ -140,15 +154,9 @@ public void actionPerformed(ActionEvent e) {
  	  
     }
     else if (bmiFinal >=29.9 && bmiFinal <=100) {
-    	 bmiFinalLabel.setText(String.valueOf("Your Bmi is " +bmiFinal + ", Your Obese"));
+    	 bmiFinalLabel.setText(String.valueOf("Your Bmi is " +bmiFinal + ", Your overweight"));
  	 
     }
-    
-    
-    
-    
-    
-    //bmiFinalLabel.setText(String.valueOf(bmiFinal));
 	
 }
  
