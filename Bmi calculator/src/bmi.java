cba6c33ef2222c243de6480159fba6ff9c74c5bf
Andrew <Andrew@LAPTOP-HGD6TG36.fios-router.home>
2020-09-21 23:33:43 -0400

    update code, still crashing, need to change how int works with empty string
    Change-Id: I48f1378b130a3295366f5844f2b3d7c705f56ad2

--- Bmi calculator/src/bmi.java
+++ Bmi calculator/src/bmi.java
@@ -7,27 +7,36 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-public class bmi {
 
-	        public static void main(String[] args) {
+
+public class bmi {
+	
+			public static JPanel myPanel;
+			public static JLabel feet;
+			public static JLabel inches;
+
+	        public static void main(String[] args ) {
+			
 		
 		    JTextField feet = new JTextField(5);
             JTextField inches = new JTextField(5);
 
-            JPanel myPanel = new JPanel();
+            myPanel = new JPanel();
             myPanel.add(new JLabel("feet:"));
             myPanel.add(feet);
             myPanel.add(Box.createHorizontalStrut(15)); // a spacer
             myPanel.add(new JLabel("inches:"));
             myPanel.add(inches);
-
-    	    int result = JOptionPane.showConfirmDialog(null, myPanel, 
+	        
+            int result = JOptionPane.showConfirmDialog(null, myPanel, 
              "How tall are you", JOptionPane.OK_CANCEL_OPTION);
-    	    if (result == JOptionPane.OK_OPTION) {
+    	    if (result == JOptionPane.OK_OPTION);
+	        
+	        
     		
-    		
-    		{
-    	   	  System.out.println(result);
+	        
+	    
+    	   	  //System.out.println(result);
     	   	
     	   	  String feetMem= feet.getText();
     	   	  int feet1 = Integer.parseInt(feetMem);
@@ -35,19 +44,13 @@ public class bmi {
     	   	
     	   
     	       String inchesMem= inches.getText();
-    	   	   int inches1 = Integer.parseInt(inchesMem); //if you enter nothing, it crashes
+    	   	   int inches1 = Integer.parseInt(inchesMem); 
     	   	
-    	       //JTextField feet = new JTextField("-5");
-    	       // 	String text = feet.getText();
-    	       //int xaxis1 = Integer.parseInt(text);
-    	    	
-		
-		       //double height= Integer.parseInt(JOptionPane.showInputDialog("how tall are you in inches"));
-		       
+    	    
 		        
 		       double weight= Integer.parseInt (JOptionPane.showInputDialog( "how much do you weigh"));
 		        
-		       //this is a place holder, its not working
+		       
 		       int footMeasurement= 12;
 		        
 		        
@@ -57,7 +60,7 @@ public class bmi {
 		        
 		       double bmiWeight= 703* weight;
 		       double bmiHeight= (int) Math.pow(adjustedHeight2, 2);
-		       int bmiFinal= (int) (bmiWeight/bmiHeight);
+		       int bmiFinal= (int) (bmiWeight/bmiHeight);{
 		      
 		        
 		       
@@ -85,17 +88,17 @@ public class bmi {
 		       
 		        
 		      
-		        
+		       }
 		       
 		        
 		      
 		        
-				
+    	    		
 				
 				
 	}
 
+	        }        	
+
 		
-	}
-}			
-}
+
